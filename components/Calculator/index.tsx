5321710adf3969271e33ac1c7645baa8db5bbff0
immortal otaku <elmouhib2003achraf@gmail.com>
2021-08-19 15:13:09 +0200

    adding caclulator head

--- components/Calculator/index.tsx
+++ components/Calculator/index.tsx
@@ -1,3 +1,18 @@
-const Caclulator = () => <div className="calculator__body"></div>;
+const Caclulator = () => (
+  <div className="calculator__container">
+    <div className="caclulator__head">
+      <h2>calc</h2>
+      <div className="theme__toggler__container">
+        <h4>Theme</h4>
+        <div className="theme__toggler">
+          <span>123</span>
+          <div className="toggle__slider">
+            <div className="toggle__button"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Caclulator;
