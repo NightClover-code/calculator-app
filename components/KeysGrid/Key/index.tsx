b0125fed8f6f44e757f01cba2f94759dd89b9f26
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-08-19 20:12:51 +0100

    adding an overlay

--- components/KeysGrid/Key/index.tsx
+++ components/KeysGrid/Key/index.tsx
@@ -7,6 +7,7 @@ interface KeyProps {
 const Key: React.FC<KeyProps> = ({ children, style }) => {
   return (
     <div className="key" style={style}>
+      <div className="overlay"></div>
       {children}
     </div>
   );
