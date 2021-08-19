886b5a953a9d3b0764a163a1256b71b703f0473c
immortal otaku <elmouhib2003achraf@gmail.com>
2021-08-19 14:20:40 +0200

    adding seo & head content

---
+++ utils/seoConfig.ts
@@ -0,0 +1,6 @@
+export const seoConfig = {
+  title: 'Welcome | Calculator app',
+  description:
+    'Simple and intuitive calculator, built for users to enjoy the modern web experience.',
+  keywords: 'calculator, calculation, website',
+};
